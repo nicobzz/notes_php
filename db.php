fcc34ff74b3991ad15779305dd28d3840f4137a1
Nicolas Bourdon <Nicolas@Nicotosh.(none)>
2023-02-10 11:46:20 +0100

    projet simple rajout de notes dans une base de donnée en php

---
+++ db.php
@@ -0,0 +1,24 @@
+<?php
+
+try{
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=digital;charset=utf8', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch(Exception $e) {
+    die('Erreur : '. $e->getMessage());
+}
+
+function get_notes_table(){
+    global $mysqlClient;
+    $sqlQuery = 'SELECT * FROM notes ORDER BY note ASC';
+    $notes_req = $mysqlClient->prepare($sqlQuery);
+    $notes_req->execute();
+    //Retourne un tableau contenant toutes les lignes du jeu 
+    $notes = $notes_req->fetchAll();
+    return $notes;
+}
+
+function add_note($nouvelle_note){
+    global $mysqlClient;
+    $sqlQuery = 'INSERT INTO notes(note) VALUES (:ajout_note)';
+    $insert_note = $mysqlClient->prepare($sqlQuery);
+    $insert_note->execute( array('ajout_note'=>$nouvelle_note) );
+}
---
+++ notes-include.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="notes-include.php" method="GET">
+<label for="note-id">entrez votre note</label>
+    <input type="text" id="note-id" name="note_entree">
+    <button type="submit">Valider</button>
+</form>
+<?php
+include_once('db.php');
+//inclue le fichier db.php une seule fois m^emesi on rappelle plusueireurs fois include_once('db.php');
+
+if( isset($_GET['note_entree']) ){
+    add_note($_GET['note_entree']);
+}
+
+$notes = get_notes_table();
+foreach($notes as $n_ligne=>$note){
+    //$notes est le tableau issu de la base de donnée
+    //$n_ligne va etre la clef de chaque élement du tableau (donc le numéro de la ligne du tableau)
+    //$note va être le sous tableau du tableau note qui contient une note 'note' ,
+    // et son identifiant 'id'
+    echo "note numéro ".$n_ligne.": ";
+    echo($note['note']).'<br>';
+   }
+?>
+</body>
+</html>
